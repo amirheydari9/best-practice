c5fdeb0c39ddcbeac77f6e0df6b7a10c4b0b41c5
amir <amirheydari9@gmail.com>
2021-09-24 01:35:45 -0700

    add angular material

--- src/app/lazy/lazy.component.ts
+++ src/app/lazy/lazy.component.ts
@@ -1,13 +1,16 @@
-import {Component, ComponentFactoryResolver, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {Component, ComponentFactoryResolver, OnDestroy, OnInit, ViewChild, ViewContainerRef} from '@angular/core';
+import {takeUntil} from 'rxjs/operators';
+import {Subject} from 'rxjs';
 
 @Component({
   selector: 'app-lazy',
   templateUrl: './lazy.component.html',
   styleUrls: ['./lazy.component.scss']
 })
-export class LazyComponent implements OnInit {
+export class LazyComponent implements OnInit, OnDestroy {
 
   message: string;
+  private destroy$ = new Subject();
 
   @ViewChild('greetComp', {read: ViewContainerRef}) private greetComp: ViewContainerRef;
 
@@ -23,11 +26,17 @@ export class LazyComponent implements OnInit {
   async loadGreetComponent() {
     this.greetComp.clear();
     const {GreetComponent} = await import('./greet/greet.component');
-    const greetComp = this.greetComp.createComponent(
+    const {instance} = this.greetComp.createComponent(
       this.cfr.resolveComponentFactory(GreetComponent)
     );
-    greetComp.instance.greetMessage = Math.random().toString();
-    greetComp.instance.sendMessageEvent.subscribe(data => this.message = data);
+    instance.greetMessage = Math.random().toString();
+    instance.sendMessageEvent.pipe().subscribe(data => this.message = data);
   }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
 
 }
