0011735f4a4735608741cc90650735e2e145b1f6
aheydari <amiheydari9@gmail.com>
2022-02-07 10:06:31 +0330

    complete binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RootStoreModule} from './root-store';
 import {HttpClientModule} from '@angular/common/http';
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 
 
 @NgModule({
